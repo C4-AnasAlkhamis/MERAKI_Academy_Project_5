0e5a92ef6cef5673e2fab7a6f18351b255646e74
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-02-01 23:36:45 +0200

    create login controler and endpoint with post req

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,23 +1,12 @@
 /** @format */
 
 const express = require("express");
-
+const login = require("../controllers/login");
 const loginRouter = express.Router();
 //dont press enter
 //write your code here
 
-
-
-
-
-
-
-
-
-
-
-
-
+loginRouter.post("/", login);
 
 //write your code here
 module.exports = loginRouter;
