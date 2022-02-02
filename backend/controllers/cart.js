c4c0aab852309513dde9624dccb37b95d9fd0efd
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-02-02 11:58:13 +0200

    create new controler (cart) post,get,delte endpoints

---
+++ backend/controllers/cart.js
@@ -0,0 +1,71 @@
+const connection = require("../database/db");
+// =================================================== // done
+
+// This function creates new cart
+const createNewCart = (req, res) => {
+  const { user_id, item_id } = req.body;
+
+  const query = `INSERT INTO carts (user_id,item_id) VALUE (?,?)`;
+  const data = [user_id, item_id];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    }
+    res.status(201).json({
+      success: true,
+      message: `cart created`,
+      result: result,
+    });
+  });
+};
+// =================================================== // done
+
+// This function returns Cart By Id
+const getCartById = (req, res) => {
+  const id = req.params.id;
+  const query = `SELECT * FROM carts join items ON carts.item_id = items.id where carts.user_id =?`;
+  const data = [id];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: `The carts with user ${id}`,
+      result: result,
+    });
+  });
+};
+
+// =================================================== // done
+
+// This function returns Cart By Id
+const deleteCartById = (req, res) => {
+  const id = req.params.id;
+  const query = `DELETE FROM carts WHERE id = ?`;
+  const data = [id];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: `The cart with ${id} deleted`,
+      result: result,
+    });
+  });
+};
+module.exports = {
+  createNewCart,
+  getCartById,
+  deleteCartById,
+};
