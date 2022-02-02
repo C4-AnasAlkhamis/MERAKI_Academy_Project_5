1f2093950d033055d3449adfc747b215b30d1c39
B27_OmarS <93876383+OmarAlShishani@users.noreply.github.com>
2022-02-02 18:46:03 +0200

    create items controller function and endpoint

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -5,19 +5,19 @@ const express = require("express");
 const itemsRouter = express.Router();
 //dont press enter
 //write your code here
+const {
+  createNewItem,
+  getAllItems,
+  deleteItemById,
+  getItemById,
+  updateItemById,
+} = require("../controllers/items");
 
-
-
-
-
-
-
-
-
-
-
-
-
+itemsRouter.post("/", createNewItem);
+itemsRouter.get("/", getAllItems);
+itemsRouter.delete("/:id", deleteItemById);
+itemsRouter.get("/id", getItemById);
+itemsRouter.put("/", updateItemById);
 
 //write your code here
 module.exports = itemsRouter;
