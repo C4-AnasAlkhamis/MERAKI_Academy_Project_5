0c67302b2e82cdb072d0bc047279365f1aba32c9
AlaaMarei <alaammarei@gmail.com>
2022-02-02 12:27:27 +0200

    inish signup user functionality with endpoint user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,17 @@
 /** @format */
 
 const express = require("express");
-const {createNewUser} = require("../controllers/register");
+const {createNewUser,getUserById,updateUserById,deleteUserById} = require("../controllers/users");
 const userRouter = express.Router();
 //dont press enter
 //write your code here
 
 
 userRouter.post("/", createNewUser);
+userRouter.get("/", getUserById);
+userRouter.put("/", updateUserById);
+userRouter.delete("/:id", deleteUserById);
+
 
 //write your code here
 module.exports = userRouter;
