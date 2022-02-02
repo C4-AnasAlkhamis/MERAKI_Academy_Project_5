547602ca828030eb74d3816a86f7f98db9fc9ae1
AlaaMarei <alaammarei@gmail.com>
2022-02-02 14:41:55 +0200

    get user by id at endpoint user/

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -13,7 +13,6 @@ const createNewUser = async (req, res) => {
   const data = [user_name, email, hashingPass, role_id];
 
   connection.query(query, data, (err, results) => {
-     
     if (err) {
       return res.status(409).json({
         success: false,
@@ -21,127 +20,112 @@ const createNewUser = async (req, res) => {
         err: err,
       });
     }
-   
+
     res.status(200).json({
       success: true,
       massage: "Success user Added",
       results: results,
     });
   });
-
 };
 
 // This function to get user by id.
-
 const getUserById = (req, res) => {
-    const id = req.query.id;
-  
-    const query = `SELECT * FROM users  WHERE id=?`;
-    const data = [id];
-  
-    connection.query(query, data, (err, results) => {
-      if (err) {
-        return res.status(500).json({
-          success: false,
-          massage: "Server Error",
-          err: err,
-        });
-      }
-      if (results.length == 0) {
-        res.status(404).json({
-          success: false,
-          massage: "The user Not found",
-        });
-      }
-      res.status(200).json({
-        success: true,
-        massage: `The user ${id}`,
-        results: results,
+  const id = req.query.id;
+
+  const query = `SELECT * FROM users  WHERE id=?`;
+  const data = [id];
+
+  connection.query(query, data, (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        massage: "Server Error",
+        err: err,
       });
-    });
-  };
-
-
-  
-
-  const updateUserById = (req, res) => {
-    const { userName , email , password  } = req.body;
-    const id = req.params.id;
-  
-    const query = `UPDATE users SET userName=?, email=?, password=? WHERE id=?;`;
-  
-    const data = [userName, email, password, id];
-  
-    connection.query(query, data, (err, results) => {
-      if (err) {
-        return res.status(404).json({
-          success: false,
-          massage: `Server error`,
-          err: err,
-        });
-      }
-      if (results.changedRows == 0) {
-        res.status(404).json({
-          success: false,
-          massage: `The user : ${id} is not found`,
-          err: err,
-        });
-      }
-     
-      res.status(201).json({
-        success: true,
-        massage: `the user updated`,
-        results: results,
+    }
+    if (results.length == 0) {
+      res.status(404).json({
+        success: false,
+        massage: "The user Not found",
       });
+    }
+    res.status(200).json({
+      success: true,
+      massage: `The user ${id}`,
+      results: results,
     });
-  };
+  });
+};
 
+// This function to update user by id.
+const updateUserById = (req, res) => {
+  const { userName, email, password } = req.body;
+  const id = req.params.id;
 
-  const deleteUserById = (req, res) => {
-    const id = req.params.id;
-  
-    const query = `UPDATE users SET is_deleted=1 WHERE id=?;`;
-  
-    const data = [id];
-  
-    connection.query(query, data, (err, results) => {
-      if (err) {
-        return res.status(500).json({
-          success: false,
-          massage: "Server Error",
-          err: err,
-        });
-      }
-      if (!results.changedRows) {
-        return res.status(404).json({
-          success: false,
-          massage: `The user: ${id} is not found`,
-          err: err,
-        });
-      }
-    
-      res.status(200).json({
-        success: true,
-        massage: `Succeeded to delete user with id: ${id}`,
-        results: results,
+  const query = `UPDATE users SET userName=?, email=?, password=? WHERE id=?;`;
+
+  const data = [userName, email, password, id];
+
+  connection.query(query, data, (err, results) => {
+    if (err) {
+      return res.status(404).json({
+        success: false,
+        massage: `Server error`,
+        err: err,
       });
+    }
+    if (results.changedRows == 0) {
+      res.status(404).json({
+        success: false,
+        massage: `The user : ${id} is not found`,
+        err: err,
+      });
+    }
+
+    res.status(201).json({
+      success: true,
+      massage: `the user updated`,
+      results: results,
     });
-  };
+  });
+};
 
+// This function to delete user by id.
+const deleteUserById = (req, res) => {
+  const id = req.params.id;
 
+  const query = `UPDATE users SET is_deleted=1 WHERE id=?;`;
 
+  const data = [id];
 
+  connection.query(query, data, (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        massage: "Server Error",
+        err: err,
+      });
+    }
+    if (!results.changedRows) {
+      return res.status(404).json({
+        success: false,
+        massage: `The user: ${id} is not found`,
+        err: err,
+      });
+    }
 
-
-
-
-
-
-
-
-
-
+    res.status(200).json({
+      success: true,
+      massage: `Succeeded to delete user with id: ${id}`,
+      results: results,
+    });
+  });
+};
 
 module.exports = {
-  createNewUser,getUserById,updateUserById,deleteUserById
+  createNewUser,
+  getUserById,
+  updateUserById,
+  deleteUserById,
 };
