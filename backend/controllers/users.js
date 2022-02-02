7daf07caaff3910d863aa93b04491bdb8e6f3fce
AlaaMarei <alaammarei@gmail.com>
2022-02-02 14:29:04 +0200

    create new user at end point user/

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,37 +1,19 @@
 const bcrypt = require("bcrypt");
 const connection = require("../database/db");
-const secretSalt = process.env.SALT;
+// const secretSalt = process.env.SALT;
+// require("dotenv").config();
 
 // This function to sign up new user .
 const createNewUser = async (req, res) => {
-  const { userName, email, password, role_id } = req.body;
+  const { user_name, email, password, role_id } = req.body;
 
   const hashingPass = await bcrypt.hash(password, 7);
 
   const query = `INSERT INTO users (user_name, email, password, role_id) VALUES (?,?,?,?)`;
-  const data = [userName, email, hashingPass, role_id];
+  const data = [user_name, email, hashingPass, role_id];
 
   connection.query(query, data, (err, results) => {
-        // if (err) throw err;
-        // if (results) {
-        //   res.status(200).json({
-        //     success: true,
-        //     message: `Success user Added`,
-        //     user: results,
-        //   });
-        // } else if (err.keyPattern) {
-        //   return res.status(409).json({
-        //     success: false,
-        //     message: `The email already exists`,
-        //   });
-        // } else {
-        //   res.status(500).json({
-        //     success: false,
-        //     message: `Server Error`,
-        //     err: err,
-        //   });
-        // }
-
+     
     if (err) {
       return res.status(409).json({
         success: false,
