e87f2c4f3a10453db66b6050d6e855a99092a8d9
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-02-03 14:24:28 +0200

    create new component Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
-import ItemInfo from "./components/ItemInfo/ItemIfom";
+import ItemInfo from "./components/ItemInfo/ItemInfo";
+import Register from "./components/register/Register";
 function App() {
   return (
     <div className="App">
       {/* <NavBar /> */}
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
       </Routes>
       <Routes>
         <Route path="/more-info" element={<ItemInfo />} />
