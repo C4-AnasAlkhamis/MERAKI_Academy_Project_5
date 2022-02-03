ffbeb8083dd0319f31300f61d76a8ac4740e2353
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-02-03 11:54:00 +0200

    update on component ItemInfo add delete function

--- frontend/src/components/ItemInfo/ItemIfom.jsx
+++ frontend/src/components/ItemInfo/ItemIfom.jsx
@@ -1,43 +1,76 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { useNavigate, LINK } from "react-router-dom";
 import "./itemInfo.css";
-import { setItemInfo } from "../../reducer/itemInfo/index";
+import { setItemInfo, updateItemInfo } from "../../reducer/itemInfo/index";
 import { useSelector, useDispatch } from "react-redux";
 
 const ItemInfo = () => {
   const dispatch = useDispatch();
+  const [message, setMessage] = useState("");
+  const [isDeleted, setIsDeleted] = useState(false);
+
   const state = useSelector((state) => {
     return { item: state.itemInfoReducer.itemInfo };
   });
-  // const getItemById = async (e) => {
-  //   e.preventDefault();
-  //   //get http://localhost:5000/item/
 
-  //   await axios
-  //     .get(`http://localhost:5000/item/${id}`)
-  //     .then((result) => {
-  //       dispatch(setItemInfo(result.data.result));
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
+  // ============================================= //
 
+  const getItemById = async () => {
+    //get http://localhost:5000/item/
+
+    await axios
+      .get(`http://localhost:5000/item/id?id=${state.item.id}`)
+      .then((result) => {
+        dispatch(setItemInfo({ ...result.data.result }));
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // ============================================= //
+
+  const deleteItemById = async () => {
+    await axios
+      .delete(`http://localhost:5000/item/${state.item.id}`)
+      .then((result) => {
+        setMessage("Item has been deleted successfully");
+        setIsDeleted(true);
+        dispatch(setItemInfo(""));
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log(err);
+        setMessage("Error happened while deleting the item");
+      });
+  };
+  // useEffect(() => {
+  //   getItemById();
+  // }, []);
   return (
-    <>
-      <div className="item_info_box">
-        <div className="img_box">
-          <img src={state.item.img} alt="" />
-        </div>
-        <div className="info_box">
-          <p>{state.item.title}</p>
-          <span>{state.item.price}</span>
-          <span>{state.item.rate}</span>
-        </div>
-      </div>
-    </>
+    <div className="item_info_box">
+      {isDeleted ? (
+        <span>{message}</span>
+      ) : (
+        <>
+          <div className="img_box">
+            <img src={state.item.img} alt="" />
+          </div>
+          <div className="info_box">
+            <p>{state.item.title}</p>
+            <span>$ {state.item.price}</span>
+            <span>{state.item.rate}</span>
+          </div>
+          <div>
+            <button onClick={deleteItemById}>delete</button>
+            {/* <button onClick={updateItemById}>update</button> */}
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
--- frontend/src/reducer/itemInfo/index.js
+++ frontend/src/reducer/itemInfo/index.js
@@ -3,6 +3,7 @@ const initialState = {
     img: "https://itslondon.s3.amazonaws.com/p/l/DEWDCH133NT.jpg",
     title: "Milwaukee M18 FN16GA-0X FUEL Angled Nail Gun 16 Gauge with C...",
     price: 300,
+    id: 1,
   },
 };
 // =======================  //
@@ -11,6 +12,9 @@ const itemInfoReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case "SET_ITEMINFO":
       return { itemInfo: payload };
+    case "UPDATE_ARTICLE":
+      return { itemInfo: payload };
+
     default:
       return state;
   }
@@ -25,16 +29,11 @@ export const setItemInfo = (ItemInfo) => {
 };
 // =======================  //
 
-// export const addArticle = (newArticle) => {
-//   return { type: "ADD_ARTICLE", payload: newArticle };
-// };
-// // =======================  //
-
-// export const updateItemInfo = (updatedItemInfo) => {
-//   return { type: "UPDATE_ITEMINFO", payload: updatedItemInfo };
-// };
+export const updateItemInfo = (updatedItemInfo) => {
+  return { type: "UPDATE_ITEMINFO", payload: updatedItemInfo };
+};
 // =======================  //
 
-// export const deleteArticles = (id) => {
-//   return { type: "DELETE_ARTICLE", payload: id };
+// export const deleteItemInfo = (id) => {
+//   return { type: "DELETE_ITEMINFO", payload: id };
 // };
