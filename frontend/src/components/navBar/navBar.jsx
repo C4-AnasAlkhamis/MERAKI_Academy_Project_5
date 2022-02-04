65838b74fdcf1cd84cf5347d243d1a578f443937
B27_OmarS <93876383+OmarAlShishani@users.noreply.github.com>
2022-02-04 15:29:55 +0200

    update css

--- frontend/src/components/navBar/navBar.jsx
+++ frontend/src/components/navBar/navBar.jsx
@@ -1,8 +1,11 @@
-import { Link } from "react-router-dom";
+/** @format */
 
+import { Link } from "react-router-dom";
+import "./navbar.css";
 import { logOut } from "../../reducer/login/index";
 import { useSelector, useDispatch } from "react-redux";
 
+import logo from "../../image/logo.png";
 const NavBar = () => {
   const { isLoggedIn } = useSelector((state) => {
     return { isLoggedIn: state.loginReducer.isLoggedIn };
@@ -11,39 +14,43 @@ const NavBar = () => {
   return (
     <>
       <div className="NavBar">
-        {isLoggedIn ? (
-          <>
-            <Link to="/homePage" style={{ textDecoration: "none" }}>
-              Home
-            </Link>
-            <Link to="/cart" style={{ textDecoration: "none" }}>
-              Cart
-            </Link>
-            <Link to="/wishlist" style={{ textDecoration: "none" }}>
-              Wishlist
-            </Link>
+        <div>
+          <img src={logo} />
+        </div>
+        <div className="Link">
+          {isLoggedIn ? (
+            <>
+              <Link to="/homePage" style={{ textDecoration: "none" }}>
+                Home
+              </Link>
+              <Link to="/cart" style={{ textDecoration: "none" }}>
+                Cart
+              </Link>
+              <Link to="/wishlist" style={{ textDecoration: "none" }}>
+                Wishlist
+              </Link>
 
-            <Link
-              to="/homePage"
-              className="logout"
-              onClick={(e) => {
-                logOut();
-                localStorage.setItem("token", "");
-              }}
-            >
-              logout
-            </Link>
-          </>
-        ) : (
-          <>
-            <Link to="/register" style={{ textDecoration: "none" }}>
-              Register{" "}
-            </Link>
-            <Link to="/login" style={{ textDecoration: "none" }}>
-              Login
-            </Link>
-          </>
-        )}
+              <Link
+                to="/homePage"
+                className="logout"
+                onClick={(e) => {
+                  logOut();
+                  localStorage.setItem("token", "");
+                }}>
+                logout
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to="/register" style={{ textDecoration: "none" }}>
+                Register{" "}
+              </Link>
+              <Link to="/login" style={{ textDecoration: "none" }}>
+                Login
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </>
   );
