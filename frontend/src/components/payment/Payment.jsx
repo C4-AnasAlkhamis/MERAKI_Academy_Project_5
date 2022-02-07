4d5d227ed093da5789f9cc032f8d2ff5eb8e81b1
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-02-07 18:09:11 +0200

    import paypal pacdage , create thefunction +1

--- frontend/src/components/payment/Payment.jsx
+++ frontend/src/components/payment/Payment.jsx
@@ -1,58 +1,62 @@
 import React, { useState } from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 const Pay = ({ items, price }) => {
-  const [isDone, setIsDone] = useState(true);
+  const [isDone, setIsDone] = useState(false);
   const [paidFor, setPaidFor] = useState(false);
   const [error, setError] = useState(null);
   const carts = items.map((item) => {
-    return 
-     
-    
+    return item.title;
   });
-    console.log(carts);
-
   return (
     <>
-      <PayPalScriptProvider
-        options={{ "client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID }}
-      >
-        <PayPalButtons
-          style={{
-            color: "silver",
-            height: 48,
-            shape: "pill",
-          }}
-          createOrder={(data, actions) => {
-            return actions.order.create({
-              purchase_units: [
-                {
-                    description: "",
-                  amount: {
-                    value: price,
-                  },
-                },
-              ],
-            });
-          }}
-          onApprove={async (data, actions) => {
-            const order = await actions.order.capture();
-            console.log("order", order);
+      {isDone ? (
+        <div>
+          <PayPalScriptProvider
+            options={{ "client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID }}
+          >
+            <PayPalButtons
+              style={{
+                color: "silver",
+                height: 48,
+                shape: "pill",
+              }}
+              createOrder={(data, actions) => {
+                return actions.order.create({
+                  purchase_units: [
+                    {
+                      amount: {
+                        value: price,
+                      },
+                    },
+                  ],
+                });
+              }}
+              onApprove={async (data, actions) => {
+                const order = await actions.order.capture();
+                console.log("order", order);
 
-            alert(`Transaction completed by ${order} `);
+                alert(
+                  `Transaction completed by ${order.payer.name.given_name} `
+                );
+              }}
+              onError={(err) => {
+                setError(err);
+                console.error("paypal error", err);
+              }}
+            />
+          </PayPalScriptProvider>
+        </div>
+      ) : null}
+
+      {isDone ? null : (
+        <button
+          onClick={() => {
+            setIsDone(!isDone);
           }}
-          onError={(err) => {
-            setError(err);
-            console.error("paypal error", err);
-          }}
-        />
-      </PayPalScriptProvider>
-      <button
-        onClick={() => {
-          setIsDone(!isDone);
-        }}
-      >
-        showPay
-      </button>
+        >
+          showPay
+        </button>
+      )}
     </>
   );
 };
