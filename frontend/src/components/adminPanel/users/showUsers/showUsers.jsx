1d76d0840136e3a6a2d6402429c31afb98a3204e
B27_OmarS <93876383+OmarAlShishani@users.noreply.github.com>
2022-02-07 22:28:39 +0200

    show users p_1

---
+++ frontend/src/components/adminPanel/users/showUsers/showUsers.jsx
@@ -0,0 +1,45 @@
+/** @format */
+
+import React, { useEffect, useState } from "react";
+import Select from "react-select";
+import "./homePage.css";
+import PaginateReact from "react-paginate";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { setItems, setCategories } from "../../reducer/item/index";
+import { setItemInfo } from "../../reducer/itemInfo/index";
+import { useNavigate } from "react-router-dom";
+
+//===============================================================
+
+const ShowUsers = () => {
+  const state = useSelector((state) => {
+    return {
+      items: state.itemsReducer.items,
+      categories: state.itemsReducer.categories,
+    };
+  });
+
+  //===============================================================
+
+  const getAllUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/user");
+      if (res.data.success) {
+        dispatch(setItems(res.data.items));
+      } else throw Error;
+    } catch (error) {
+      if (!error.response.data.success) {
+        return setMessage(error.response.data.message);
+      }
+      setMessage("Error happened while Get Data, please try again");
+    }
+  };
+  //===============================================================
+
+  return <div className="showUsers">
+
+  </div>;
+};
+
+export default ShowUsers;
