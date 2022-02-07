2df07a3f3e82cc9cdeed2aa5e15c38b76aa0e14d
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-02-07 22:01:40 +0200

    start with category admin component

--- frontend/src/components/payment/Payment.jsx
+++ frontend/src/components/payment/Payment.jsx
@@ -34,6 +34,7 @@ const Pay = ({ items, price }) => {
                         value: price,
                       },
                     },
+                    
                   ],
                 });
               }}
