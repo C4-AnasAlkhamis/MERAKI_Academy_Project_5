38804b1b8e4f2cd49cf4557f1a2528e6dae2d873
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-02-08 11:16:17 +0200

    create component Acategory with get all gategory

--- frontend/src/components/adminPanel/categories/Caregories.jsx
+++ frontend/src/components/adminPanel/categories/Caregories.jsx
@@ -1,8 +1,14 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-// import { setCategories } from "../../reducer/item/index";
+import { setCategories } from "../../../reducer/item/index";
+import { BiPencil } from "react-icons/bi";
+import { FcDeleteRow } from "react-icons/fc";
 
 const Category = () => {
   const dispatch = useDispatch();
+  const [message, setMessage] = useState();
 
   const state = useSelector((state) => {
     return {
@@ -10,11 +16,54 @@ const Category = () => {
       categories: state.itemsReducer.categories,
     };
   });
-  const { categories, token, items } = state;
-  console.log(categories);
+  const { categories, token } = state;
+
+  //===============================================================
+  const getAllCategories = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/category", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (res.data.success) {
+        dispatch(setCategories(res.data.result));
+      } else throw Error;
+    } catch (error) {
+      if (!error.response.data.success) {
+        return setMessage(error.response.data.message);
+      }
+      setMessage("Error happened while Get Data, please try again");
+    }
+  };
+  useEffect(() => {
+    getAllCategories();
+  }, []);
+
   return (
     <>
       <h1>Category panel</h1>
+      <table>
+        <tr>
+          <th>id</th>
+          <th>category</th>
+        </tr>
+        {categories.map((category) => {
+          console.log(category);
+          return (
+            <tr>
+              <td>{category.id}</td>
+              <td>{category.category}</td>
+              <td>
+                <BiPencil />
+              </td>
+              <td>
+                <FcDeleteRow />
+              </td>
+            </tr>
+          );
+        })}
+      </table>
     </>
   );
 };
