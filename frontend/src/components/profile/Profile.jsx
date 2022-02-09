52380f72509874284f453797919f819ecc981f77
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-02-09 12:49:46 +0200

    frontend profile getServiceBiuserId function

--- frontend/src/components/profile/Profile.jsx
+++ frontend/src/components/profile/Profile.jsx
@@ -2,6 +2,12 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import {
+  setService,
+  updateService,
+  deleteService,
+} from "../../reducer/service/index";
+
 const Profile = () => {
   const [message, setMessage] = useState();
   const navigate = useNavigate();
@@ -13,7 +19,19 @@ const Profile = () => {
   });
   const dispatch = useDispatch();
   //===============================================================
+  const getServiceByUserId = async (id) => {
+    //get http://localhost:5000/setvice/id
 
+    await axios
+      .get(`http://localhost:5000/service/user/${id}`)
+      .then((result) => {
+        dispatch(setServiceInfo({ ...result.data.result }));
+        navigate("/service-info");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return <div> Profile</div>;
 };
 
