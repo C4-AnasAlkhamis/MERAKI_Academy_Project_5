09fe21b8ce25b184bd9a05929a6dd30dc4fc8eaa
AlaaMarei <alaammarei@gmail.com>
2022-02-09 16:30:21 +0200

    create worker roueter and create new worker

--- backend/controllers/worker.js
+++ backend/controllers/worker.js
@@ -1,19 +1,87 @@
 const connection = require("../database/db");
 
 // This function creates new Service
-const createNewWorker = (req, res) => {};
+const createNewWorker = (req, res) => {
+
+  const { user_id, service_id, address,phone,image } = req.body;
+
+  const query = `INSERT INTO services (user_id, service_id, address,phone,image ) VALUE (?,?,?,?,?)`;
+  const data = [ user_id, service_id, address,phone,image ];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    }
+    res.status(201).json({
+      success: true,
+      message: `Success Worker Created`,
+      result: result,
+    });
+  });
+
+
+};
 
 // This function returns all services
-const getAllWorkers = (req, res) => {};
+const getAllWorkers = (req, res) => {
+
+  const query = `SELECT * FROM worker `;
+  connection.query(query, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    }
+    if (!result) {
+      return res.status(200).json({
+        success: false,
+        message: `No Worker Yet`,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: `All the Worker Available`,
+      result: result,
+    });
+  });
+
+
+
+
+};
 
 // This function returns Service By Id
-const getWorkerById = (req, res) => {};
+const getWorkerById = (req, res) => {
+
+
+
+
+};
 
 // This function to update Service by id
-const updateWorkerById = (req, res) => {};
+const updateWorkerById = (req, res) => {
+
+ 
+
+
+
+
+
+
+};
 
 // This function to delete Service By Id
-const deleteWorkerById = (req, res) => {};
+const deleteWorkerById = (req, res) => {
+ 
+
+
+
+
+
+};
 module.exports = {
   createNewWorker,
   getAllWorkers,
--- frontend/src/reducer/service/index.js
+++ frontend/src/reducer/service/index.js
@@ -1,65 +1,65 @@
-/** @format */
+// /** @format */
 
-const initialState = {
-  services: [],
-  serviceInfo ={}
-};
-// =======================  //
+// const initialState = {
+//   services: [],
+//   serviceInfo ={}
+// };
+// // =======================  //
 
-const serviceReducer = (state = initialState, { type, payload }) => {
-  switch (type) {
-    case "SET_SERVICE":
-      return { ...state, services: payload };
+// const serviceReducer = (state = initialState, { type, payload }) => {
+//   switch (type) {
+//     case "SET_SERVICE":
+//       return { ...state, services: payload };
 
-    case "SET_SERVICEINFO":
-      return { ...state, serviceInfo: payload };
+//     case "SET_SERVICEINFO":
+//       return { ...state, serviceInfo: payload };
 
-    case "ADD_SERVICE":
-      return { ...state, services: [...state.services, payload] };
+//     case "ADD_SERVICE":
+//       return { ...state, services: [...state.services, payload] };
 
-    case "DELETE_SERVICE":
-      return {
-        ...state,
-        services: state.services.filter((service) => {
-          return service.id !== payload;
-        }),
-      };
-    case "UPDATE_SERVICE":
-      return {
-        ...state,
-        services: state.services.map((service) => {
-          if (service.id === payload.id) {
-            return payload;
-          }
-          return service;
-        }),
-      };
+//     case "DELETE_SERVICE":
+//       return {
+//         ...state,
+//         services: state.services.filter((service) => {
+//           return service.id !== payload;
+//         }),
+//       };
+//     case "UPDATE_SERVICE":
+//       return {
+//         ...state,
+//         services: state.services.map((service) => {
+//           if (service.id === payload.id) {
+//             return payload;
+//           }
+//           return service;
+//         }),
+//       };
 
-    default:
-      return state;
-  }
-};
+//     default:
+//       return state;
+//   }
+// };
 
-export default serviceReducer;
+// export default serviceReducer;
 
-export const setService = (services) => {
-  return { type: "SET_SERVICE", payload: services };
-};
-// =======================  //
-export const setServiceInfo = (servicesInfo) => {
-  return { type: "SET_SERVICEINFO", payload: servicesInfo };
+// export const setService = (services) => {
+//   return { type: "SET_SERVICE", payload: services };
+// };
+// // =======================  //
+// export const setServiceInfo = (servicesInfo) => {
+//   return { type: "SET_SERVICEINFO", payload: servicesInfo };
  
-};
-export const addService = (newService) => {
-  return { type: "ADD_SERVICE", payload: newService };
-};
-// =======================  //
+// };
+// export const addService = (newService) => {
+//   return { type: "ADD_SERVICE", payload: newService };
+// };
+// // =======================  //
 
-export const updateService = (newService) => {
-  return { type: "UPDATE_SERVICE", payload: newService };
-};
-// =======================  //
+// export const updateService = (newService) => {
+//   return { type: "UPDATE_SERVICE", payload: newService };
+// };
+// // =======================  //
 
-export const deleteService = (id) => {
-  return { type: "DELETE_SERVICE", payload: id };
-};
+// export const deleteService = (id) => {
+//   return { type: "DELETE_SERVICE", payload: id };
+// };
