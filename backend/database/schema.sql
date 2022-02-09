96713aa77be9dd37530cd8a74b228fec90b9cc3d
AlaaMarei <alaammarei@gmail.com>
2022-02-09 10:47:52 +0200

    update schema

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -12,81 +12,81 @@ USE MERAKI_Academy_Project_5;
 -- DROP TABLE services;
 
 -- ============================ // done
--- CREATE TABLE permissions (
---     id INT AUTO_INCREMENT NOT NULL,
---     permission VARCHAR(100) NOT NULL,
---     PRIMARY KEY (id)
--- );
+CREATE TABLE permissions (
+    id INT AUTO_INCREMENT NOT NULL,
+    permission VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
 
 -- ============================ // done
--- CREATE TABLE roles (
---     id INT AUTO_INCREMENT NOT NULL,
---     role VARCHAR(100) NOT NULL,
---     PRIMARY KEY (id)
--- );
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
+    role VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
 
 -- ============================ // done 
--- CREATE TABLE role_permission (
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     role INT NOT NULL,
---     permission INT NOT NULL,
---     FOREIGN KEY (role) REFERENCES roles (id),
---     FOREIGN KEY (permission) REFERENCES permissions (id)
--- );
+CREATE TABLE role_permission (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role INT NOT NULL,
+    permission INT NOT NULL,
+    FOREIGN KEY (role) REFERENCES roles (id),
+    FOREIGN KEY (permission) REFERENCES permissions (id)
+);
 
 -- ============================ // done
--- CREATE TABLE users(
---     id INT AUTO_INCREMENT NOT NULL,
---     user_name VARCHAR(100) NOT NULL,
---     email VARCHAR(100) UNIQUE NOT NULL,
---     password VARCHAR(255) NOT NULL,
---     role_id INT,
---     FOREIGN KEY (role_id) REFERENCES roles(id),
---     is_deleted TINYINT DEFAULT 0,
---     PRIMARY KEY (id)
--- );
+CREATE TABLE users(
+    id INT AUTO_INCREMENT NOT NULL,
+    user_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (id)
+);
 
 -- user name VARCHAR(100),
 -- ============================ // done
--- CREATE TABLE categories (
---     id INT AUTO_INCREMENT NOT NULL,
---     category VARCHAR(100) NOT NULL,
---     PRIMARY KEY (id)
--- );
+CREATE TABLE categories (
+    id INT AUTO_INCREMENT NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
 
 -- ============================ // done
--- CREATE TABLE items (
---     id INT AUTO_INCREMENT NOT NULL,
---     title VARCHAR(255),
---     descriptions VARCHAR(255),
---     category_id INT,
---     img VARCHAR(250) NOT NULL,
---     price int NOT NULL,
---     rate INT,
---     FOREIGN KEY (category_id) REFERENCES categories(id),
---     PRIMARY KEY (id),
---     is_deleted TINYINT DEFAULT 0
--- );
+CREATE TABLE items (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(255),
+    descriptions VARCHAR(255),
+    category_id INT,
+    img VARCHAR(250) NOT NULL,
+    price int NOT NULL,
+    rate INT,
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    PRIMARY KEY (id),
+    is_deleted TINYINT DEFAULT 0
+);
 
 -- ============================ // done 
--- CREATE TABLE carts (
---     id INT NOT NULL AUTO_INCREMENT,
---     item_id INT NOT NULL,
---     user_id INT NOT NULL,
---     PRIMARY KEY (id),
---     FOREIGN KEY (item_id) REFERENCES items (id),
---     FOREIGN KEY (user_id) REFERENCES users (id)
--- );
+CREATE TABLE carts (
+    id INT NOT NULL AUTO_INCREMENT,
+    item_id INT NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (item_id) REFERENCES items (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
 
 -- ============================ // done 
--- CREATE TABLE wishlists (
---     id INT NOT NULL AUTO_INCREMENT,
---     item_id INT NOT NULL,
---     user_id INT NOT NULL,
---     PRIMARY KEY (id),
---     FOREIGN KEY (item_id) REFERENCES items (id),
---     FOREIGN KEY (user_id) REFERENCES users (id)
--- );
+CREATE TABLE wishlists (
+    id INT NOT NULL AUTO_INCREMENT,
+    item_id INT NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (item_id) REFERENCES items (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
 
 CREATE TABLE services (
     id INT AUTO_INCREMENT NOT NULL,
