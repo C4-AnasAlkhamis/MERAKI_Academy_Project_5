1ebfed50ec4f644ee073f031c4188392c00b9668
B27_OmarS <93876383+OmarAlShishani@users.noreply.github.com>
2022-02-12 21:52:06 +0200

    profile style

--- frontend/src/components/profile/Profile.jsx
+++ frontend/src/components/profile/Profile.jsx
@@ -228,7 +228,7 @@ const Profile = () => {
               uploadImage();
               setShow(false);
             }}>
-            Add Service
+            Done Editing
           </button>
 
           <br />
