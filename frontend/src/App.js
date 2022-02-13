181167a419f8f20119fc7fca3b77dfa14fca0f3d
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-02-13 20:41:50 +0200

    start of chat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -80,7 +80,6 @@ function App() {
           element={
             <>
               <ServicePage />
-              <Footer />
             </>
           }
         />
@@ -100,7 +99,6 @@ function App() {
       <div>
         <>{/* <FeedBack /> */}</>
       </div>
-
     </div>
   );
 }
