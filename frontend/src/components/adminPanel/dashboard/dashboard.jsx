37d383b47d9d341f2ac2981b455be9179fda9cdf
B27_OmarS <93876383+OmarAlShishani@users.noreply.github.com>
2022-02-14 20:43:13 +0200

    creat dashboard

--- frontend/src/components/adminPanel/dashboard/dashboard.jsx
+++ frontend/src/components/adminPanel/dashboard/dashboard.jsx
@@ -1,57 +1,20 @@
 /** @format */
 
 import React from "react";
-import { Chart } from "react-charts";
+import { Bar } from "react-chartjs-2";
 
 const Dashboard = () => {
-  //   const data = React.useMemo(
-  //     () => [
-  //       {
-  //         label: "Series 1",
-  //         data: [
-  //           [0, 1],
-  //           [1, 2],
-  //           [2, 4],
-  //           [3, 2],
-  //           [4, 7],
-  //         ],
-  //       },
-  //       {
-  //         label: "Series 2",
-  //         data: [
-  //           [0, 3],
-  //           [1, 1],
-  //           [2, 5],
-  //           [3, 6],
-  //           [4, 4],
-  //         ],
-  //       },
-  //     ],
-  //     []
-  //   );
-
-  //   const axes = React.useMemo(
-  //     () => [
-  //       { primary: true, type: "linear", position: "bottom" },
-  //       { type: "linear", position: "left" },
-  //     ],
-  //     []
-  //   );
-
-  //   const lineChart = (
-  //     // A react-chart hyper-responsively and continuously fills the available
-  //     // space of its parent element automatically
-  //     <div
-  //       style={{
-  //         width: "400px",
-  //         height: "300px",
-  //       }}>
-  //       <Chart data={data} axes={axes} />
-  //     </div>
-  //   );
-  return <div className="dashboard">
-
-  </div>;
+  return (
+    <div className="dashboard">
+      <Bar
+        data={{
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        }}
+        height={400}
+        width={600}
+      />
+    </div>
+  );
 };
 
 export default Dashboard;
