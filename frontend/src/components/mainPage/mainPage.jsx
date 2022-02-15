955a929c82c7c6b416b8b1a2d6dc36a0fb0cff9b
B27_OmarS <93876383+OmarAlShishani@users.noreply.github.com>
2022-02-15 17:59:19 +0200

    add images

--- frontend/src/components/mainPage/mainPage.jsx
+++ frontend/src/components/mainPage/mainPage.jsx
@@ -2,6 +2,24 @@
 
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import headMainImag from "../../image/Main/slider-d2.jpg";
+import img_1 from "../../image/Main/2pieceKits.png";
+import img_2 from "../../image/Main/Batteries.jpg";
+import img_3 from "../../image/Main/Chargers.jpg";
+
+import  bosch from "../../image/Main/bosch.png";
+import dewalt from "../../image/Main/dewalt";
+import einhell from "../../image/Main/einhell";
+import festool from "../../image/Main/festool";
+import jcb from "../../image/Main/jcb";
+import mafell from "../../image/Main/mafell";
+import makita from "../../image/Main/makita";
+import milwaukee from "../../image/Main/milwaukee";
+import panasonic from "../../image/Main/panasonic";
+import pasload from "../../image/Main/pasload";
+import ryobi from "../../image/Main/ryobi";
+import tjep from "../../image/Main/tjep";
+import worx from "../../image/Main/worx";
 
 const MainPage = () => {
   const [feedbacks, setFeedbacks] = useState([]);
@@ -25,9 +43,20 @@ const MainPage = () => {
 
   return (
     <div className="mainPage">
-      <h1>Main</h1>
+      <div className="Hadar">
+        <img src={headMainImag} />
+      </div>
+      <div className="showImg">
+        <img src={img_1} />
+        <img src={img_2} />
+        <img src={img_3} />
+      </div>
+      <div className="showImg">
+        <img src={img_1} />
+        <img src={img_2} />
+        <img src={img_3} />
+      </div>
 
-      
       {/* {feedbacks.map((feedback,i) => {
         return (
           <>
