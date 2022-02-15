ecb13488f56e02c7194f6f180713eba41d303eb0
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-02-15 12:10:06 +0200

    style popup button

--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -4,43 +4,38 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 import logo from "../../image/cones.jpg";
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 const Register = () => {
-
-  const rightRegister =()=>{
-
+  const rightRegister = () => {
     Swal.fire({
-      icon: 'success',
-      title: 'Your Account has been Created!',
+      icon: "success",
+      title: "Your Account has been Created!",
       showConfirmButton: false,
-      timer: 3500
-    })
-  }
+      timer: 1500,
+    });
+  };
 
-const wrongRegister =()=>{
-  Swal.fire({
-    icon: 'error',
-    title: 'Oops...',
-    text: ' Fill All Required Inputs ..Please!',
-  })
-
-}
+  const wrongRegister = (message) => {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: message,
+      confirmButtonColor: "#3085d6",
+    });
+  };
   const navigate = useNavigate();
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
-  const [message, setMessage] = useState("");
   const [done, setDone] = useState(false);
-  
+
   const createUser = async (e) => {
     e.preventDefault();
     //   POST -> http://localhost:5000/user
     if (!userName || !email || !password || !repeatPassword) {
-      wrongRegister()
-
-      setMessage("please fill in all inputs");
+      wrongRegister("please fill in all inputs");
     } else if (repeatPassword === password) {
       await axios
         .post("http://localhost:5000/user", {
@@ -49,9 +44,8 @@ const wrongRegister =()=>{
           password,
         })
         .then((result) => {
-          if (result) 
-          rightRegister()
-          setMessage(result.data.message);
+          console.log(result);
+          if (result) rightRegister();
           setUserName("");
           setRepeatPassword("");
           setEmail("");
@@ -60,62 +54,62 @@ const wrongRegister =()=>{
           navigate("/login");
         })
         .catch((err) => {
-          setMessage(err.response.data.message);
+          wrongRegister("The Email already exists");
         });
     } else {
-      setMessage("The password should be the same in the repeat password");
+      wrongRegister("The password should be the same in the repeat password");
     }
   };
   return (
     <div className="register">
       <div className="group">
-      <div className="regImg">
+        <div className="regImg">
           <img src={logo} />
         </div>
-      <form onSubmit={createUser}>
-        <input
-          onChange={(e) => {
-            setUserName(e.target.value);
-          }}
-          value={userName}
-          type="text"
-          placeholder="UserName"
-        />
+        <form onSubmit={createUser}>
+          <input
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+            value={userName}
+            type="text"
+            placeholder="UserName"
+          />
 
-        <input
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-          value={email}
-          type="email"
-          placeholder="Email"
-        />
-        <input
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          value={password}
-          type="password"
-          placeholder="Password"
-        />
-        <input
-          onChange={(e) => {
-            setRepeatPassword(e.target.value);
-          }}
-          value={repeatPassword}
-          type="Password"
-          placeholder="Repeat password"
-        />
-        <button>Register</button>
-        <span
-          style={{
-            color: `${done ? "#24dc3a" : "#dc2424"}`,
-            textShadow: `1px 0 1px  ${done ? "#24dc3a" : "#dc2424"}`,
-          }}
-        >
-          {/* {message} */}
-        </span>
-      </form>
+          <input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            value={email}
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            value={password}
+            type="password"
+            placeholder="Password"
+          />
+          <input
+            onChange={(e) => {
+              setRepeatPassword(e.target.value);
+            }}
+            value={repeatPassword}
+            type="Password"
+            placeholder="Repeat password"
+          />
+          <button>Register</button>
+          <span
+            style={{
+              color: `${done ? "#24dc3a" : "#dc2424"}`,
+              textShadow: `1px 0 1px  ${done ? "#24dc3a" : "#dc2424"}`,
+            }}
+          >
+            {/* {message} */}
+          </span>
+        </form>
       </div>
     </div>
   );
