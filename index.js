717d8c71a47559992f47144afec2b150b67e62a2
B27_OmarS <93876383+OmarAlShishani@users.noreply.github.com>
2022-02-16 21:17:07 +0200

    add path and rename server to index

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ const socket = require("socket.io");
 require("dotenv").config();
 const app = express();
 const db = require("./database/db");
+const path=require("path")
+
 
 app.use(cors());
 
 app.use(express.json());
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = 5000;
 
